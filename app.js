bb45fd5b60fe10d40ece49a045416dc14283a7b4
ZenDevvv <148423618+ZenDevvv@users.noreply.github.com>
2023-11-17 17:17:45 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const socket = io('http://localhost:3000')
+const socket = io('https://scidamaonline.onrender.com')
 const gameBoard = document.querySelector("#gameboard")
 const playerDisplay = document.querySelector("#player")
 const infoStatus = document.querySelector("#info")
